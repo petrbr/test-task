0d83d58af8fc2d8400cf2d215e2f4dccb96435c6
petrbr <33488156+petrbr@users.noreply.github.com>
2017-11-08 23:42:11 +0100

    Final edit

--- app/app.jsx
+++ app/app.jsx
@@ -3,12 +3,12 @@ import Search from './search';
 
 class App extends React.Component {
 
-  render() {
-	return (
-	  <div className="main">
-		<Search />
-	  </div>
-	);
-  }
+	render() {
+		return (
+			<div className="main">
+				<Search />
+			</div>
+		);
+	}
 }
 export default App;
--- app/greeter.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class Greeter extends React.Component {
-  render() {
-    return <h1>Hellddssssso!</h1>;
-  }
-}
-
-export default Greeter;
